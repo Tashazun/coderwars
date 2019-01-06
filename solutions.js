9cf6e7c70e21403eaf82b56c785445166f634bc9
Tasha R Zuniga <Tashazun@gmail.com>
2019-01-06 13:20:16 -0800

    7 kyu kata solution

--- solutions.js
+++ solutions.js
@@ -33,3 +33,18 @@ function rgb(r, g, b){
     return hex.toUpperCase();
     
   }
+
+// Sum of Odd Cubed Numbers
+
+function cubeOdd(arr) {
+
+    const answer = arr.filter(x => x % 2 !== 0)
+        .map(x => (x * x) * x)
+        .reduce((a, b) => a + b);
+
+    if (isNaN(answer)) {
+      return undefined;
+    } else {
+    return answer;
+    }
+  }
